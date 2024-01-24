9f99d0d8e53017d3b64775a4f9afa85584ebbb40
jkup14 <jkup14@gmail.com>
2024-01-24 17:31:12 -0500

    ddp rollout working, i think backward pass has a bug

--- Single_Integrator_test.cpp
+++ Single_Integrator_test.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <Eigen/Cholesky>
-#include "integrator.cpp"
-#include "solution_struct.cpp"
-#include "cost.cpp"
+#include "ddp.cpp"
 
 //TODO eigen print formatting perhaps
 
 int main() {
-    const int T = 3;
+    const int T = 10;
+    float dt = 0.1;
     const typedef float type;
     const SingleIntegrator<T> Model = SingleIntegrator<T>();
     const int nx = Model.getNx();
     const int nu = Model.getNu();
 
-    auto x0 = Eigen::Matrix<float, nx, 1>::Zero();
-    Eigen::MatrixXd u0 = Eigen::MatrixXd::Zero(T, nu);
+    Eigen::Matrix<float,nx,1> x0 = Eigen::Matrix<float, nx, 1>::Zero();
+    Eigen::Matrix<float,T,nx> X = Eigen::Matrix<float, T, nx>::Zero();
+    Eigen::Matrix<float,1,nx> x_goal = (Eigen::Matrix<float,1,nx>()<<1,2).finished();
+    Eigen::Matrix<float,T,nx> X_track = x_goal.replicate<T,1>();
+    Eigen::Matrix<float,T-1,nu> U = Eigen::Matrix<float, T-1, nu>::Constant(0);
     // std::cout << x0 << std::endl;
     // std::cout << u0 << std::endl;
 
@@ -22,18 +24,18 @@ int main() {
     std::cout << Model.xdot(x0, (Eigen::Matrix<float, nu, 1>()<<1,2).finished()) << std::endl;
 
     //Test differentiate dynamics and << override
-    Dynamics_Derivatives_Struct<T, nx, nu> dds;
-    Model.differentiate_dynamics(Eigen::Matrix<float, T, nx>::Random(), Eigen::Matrix<float, T-1, nu>::Random(), dds);
-    std::cout << dds << std::endl;
+    Dynamics_Jacobians_Struct<T, nx, nu> djs;
+    Model.differentiate_dynamics(X, U, djs);
+    std::cout << djs << std::endl;
 
     //Test Integrator
-    EulerIntegrator<T, nx, nu> Dyn(Model, 0.1);
+    EulerIntegrator<T, nx, nu> Dyn(Model, dt);
     std::cout << "xnext:" << endl << Dyn.propogate(x0, (Eigen::Matrix<float, nu, 1>()<<1,2).finished()) << std::endl;
 
     // //Test differentiate integrator and << override
-    Integrator_Derivatives_Struct<T, nx, nu> ids;
-    Dyn.differentiate_integrator(Eigen::Matrix<float, T, nx>::Random(), Eigen::Matrix<float, T-1, nu>::Random(), ids);
-    std::cout << ids << std::endl;
+    Integrator_Jacobians_Struct<T, nx, nu> ijs;
+    Dyn.differentiate_integrator(X, U, ijs);
+    std::cout << ijs << std::endl;
 
     //Test cost
     auto Q = Eigen::Matrix<float, nx, nx>::Identity()*0;
@@ -41,12 +43,37 @@ int main() {
     auto Qf = Eigen::Matrix<float, nx, nx>::Identity()*1;
     QuadraticCost<T, nx, nu> cost(Q, R, Qf);
     cout << "Cost:" << endl;
-    cout << cost.cost(Eigen::Matrix<float, T, nx>::Constant(0), Eigen::Matrix<float, T-1, nu>::Constant(1), Eigen::Matrix<float, T, nx>::Constant(0)) << endl;
-    Cost_Derivatives_Struct<T, nx, nu> cds;
-    cost.differentiate_cost(Eigen::Matrix<float, T, nx>::Constant(0), Eigen::Matrix<float, T-1, nu>::Constant(1), Eigen::Matrix<float, T, nx>::Constant(0), cds);
-    std::cout << cds << std::endl;
+    cout << cost.cost(X, U, X_track) << endl;
+    Cost_Jacobians_Struct<T, nx, nu> cjs;
+    cost.differentiate_cost(X, U, X_track, cjs);
+    std::cout << cjs << std::endl;
 
-    // DDP:DDP ddp_solver(Dyn, )
+    //Initialize Solver
+    DDP::DDP_Solver<T, nx, nu> solver(Dyn, cost);
+    
+    //Test Rollout
+    solver.rollout_controls(x0, U, X);
+    cout << "X:" << endl << X << endl;
+
+    //Test DDP
+    Value_Jacobians_Struct<nx, nu> vjs(cjs.Lx.row(T-1), cjs.Lxx.back());
+    Feedback_Struct<T, nx, nu> fs;
+    // cout << vjs << endl << fs << endl;
+    DDP::Solution<T, nx, nu> sol(Eigen::Matrix<float, T, nx>::Constant(0), 
+                                Eigen::Matrix<float, T-1, nu>::Constant(0),
+                                fs.K, std::vector<float>(5, 0));
+    // cout << sol << endl;
+    
+    std::pair<float, float> deltaVs = {0,0};
+    solver.backward_pass(cjs, ijs, vjs, fs, 0, deltaVs);
+    cout << fs << endl;
+    cout << "deltaV:" << endl << deltaVs.first << "," << deltaVs.second <<endl;
+
+    Eigen::Matrix<float,T,nx> Xnew = Eigen::Matrix<float, T, nx>::Zero();
+    Eigen::Matrix<float,T-1,nu> Unew = Eigen::Matrix<float, T-1, nu>::Zero();
+    solver.rollout_ddp_gains(x0, X, U, Xnew, Unew, fs, 1);
+    cout << "Xnew:" << endl << Xnew << endl;
+    cout << "Unew:" << endl << Unew << endl;
 
     // DDP::Solution<T_, nx, nu> sol(
     //     Eigen::Matrix<float, T_, nx>::Zero(), 
--- cost.cpp
+++ cost.cpp
@@ -6,43 +6,10 @@ using std::endl;
 // TODO: maybe store X_diff for performance isntead of calculating twice
 
 template <int T, int nx, int nu, typename type=float>
-struct Cost_Derivatives_Struct {
-    Cost_Derivatives_Struct () {
-        Lx = Eigen::Matrix<type, T, nx>::Zero();
-        Lu = Eigen::Matrix<type, T-1, nu>::Zero();
-        Lxx = std::vector<Eigen::Matrix<type, nx, nx> >(T, Eigen::Matrix<type, nx, nx>::Zero());
-        Luu =  std::vector<Eigen::Matrix<type, nu, nu> >(T-1, Eigen::Matrix<type, nu, nu>::Zero());
-        Lxu = std::vector<Eigen::Matrix<type, nx, nu> >(T-1, Eigen::Matrix<type, nx, nu>::Zero());
-    }
-    Eigen::Matrix<type, T, nx> Lx;
-    Eigen::Matrix<type, T-1, nu> Lu;
-    std::vector<Eigen::Matrix<type, nx, nx> > Lxx;
-    std::vector<Eigen::Matrix<type, nu, nu> > Luu;
-    std::vector<Eigen::Matrix<type, nx, nu> > Lxu;
-
-    template <int T_, int nx_, int nu_, typename type_>
-    friend ostream& operator<<(ostream& os, const Cost_Derivatives_Struct<T_, nx_, nu_, type_>& cds);
-};
-
-template <int T, int nx, int nu, typename type=float>
-ostream& operator<<(ostream& os, const Cost_Derivatives_Struct<T, nx, nu, type>& cds) {
-    os << "Lx:" << endl << cds.Lx << endl;
-    os << "Lu:" << endl << cds.Lu << endl;
-    os << "Lxx:" << endl;
-    for (int t = 0; t<T; t++) {os << cds.Lxx.at(t) << endl;}
-    os << "Luu:" << endl;
-    for (int t = 0; t<T-1; t++) {os << cds.Luu.at(t) << endl;}
-    os << "Lxu:" << endl;
-    for (int t = 0; t<T-2; t++) {os << cds.Lxu.at(t) << endl;}
-    os << cds.Lxu.at(T-2);
-    return os;
-}
-
-template <int T, int nx, int nu, typename type=float>
 class Cost {
     virtual type cost(const Eigen::Matrix<type, T, nx>& X, const Eigen::Matrix<type, T-1, nu>& U, const Eigen::Matrix<type, T, nx> X_track) const = 0;
 
-    virtual void differentiate_cost(const Eigen::Matrix<type, T, nx>& X, const Eigen::Matrix<type, T-1, nu>& U, const Eigen::Matrix<type, T, nx> X_track, Cost_Derivatives_Struct<T, nx, nu, type>& cds) const = 0;
+    virtual void differentiate_cost(const Eigen::Matrix<type, T, nx>& X, const Eigen::Matrix<type, T-1, nu>& U, const Eigen::Matrix<type, T, nx> X_track, Cost_Jacobians_Struct<T, nx, nu, type>& cjs) const = 0;
 };
 
 template <int T, int nx, int nu, typename type=float>
@@ -62,17 +29,17 @@ class QuadraticCost : public Cost<T, nx, nu, type> {
             return sum;
         }
 
-        void differentiate_cost(const Eigen::Matrix<type, T, nx>& X, const Eigen::Matrix<type, T-1, nu>& U, const Eigen::Matrix<type, T, nx> X_track, Cost_Derivatives_Struct<T, nx, nu, type>& cds) const {
+        void differentiate_cost(const Eigen::Matrix<type, T, nx>& X, const Eigen::Matrix<type, T-1, nu>& U, const Eigen::Matrix<type, T, nx> X_track, Cost_Jacobians_Struct<T, nx, nu, type>& cjs) const {
             const Eigen::Matrix<type, T, nx> X_diff = X-X_track;
             for (int t = 0; t<T-1; t++) {
-                cds.Lx.row(t) = X_diff.row(t) * Q;
-                cds.Lu.row(t) = U.row(t) * R;
-                cds.Lxx.at(t) = Q;
-                cds.Luu.at(t) = R;
-                cds.Lxu.at(t) = Eigen::Matrix<type, nx, nu>::Zero();
+                cjs.Lx.row(t) = X_diff.row(t) * Q;
+                cjs.Lu.row(t) = U.row(t) * R;
+                cjs.Lxx.at(t) = Q;
+                cjs.Luu.at(t) = R;
+                cjs.Lxu.at(t) = Eigen::Matrix<type, nx, nu>::Zero();
             }
-            cds.Lx.row(T-1) = X_diff.row(T-1) * Qf;
-            cds.Lxx.at(T-1) = Qf;
+            cjs.Lx.row(T-1) = X_diff.row(T-1) * Qf;
+            cjs.Lxx.at(T-1) = Qf;
         }
 
     private:
