61a2815495c2311cb9bc1656eb00e2d18166b32a
jkup14 <jkup14@gmail.com>
2024-02-23 17:41:59 -0500

    build deleted, gitignore still not working some reason

--- build/CMakeFiles/Single_Integrator.dir/source/sign_func.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Single_Integrator.dir/source/sign_func.cpp.o: \
-  /Users/joshuakuperman/Desktop/Research/DDP_CPP/source/sign_func.cpp
--- build/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o.d
+++
@@ -1,3 +0,0 @@
-matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o: \
-  /Users/joshuakuperman/Desktop/Research/DDP_CPP/matplotplusplus/source/matplot/util/popen.cpp \
-  /Users/joshuakuperman/Desktop/Research/DDP_CPP/matplotplusplus/source/matplot/util/popen.h
